64483b6c0ab89480e826fb4689fc37920ab79dfd
dev <dev@pop-os.localdomain>
2020-06-11 23:39:14 +0300

    First

---
+++ bootstrap.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# check if a go version is set
+
+clear
+echo "#################"
+echo $0 $1
+echo "#################"
+
+nodeIp=$1
+
+ls /usr/local/share/ca-certificates/PCAcer.crt > /dev/null 2>&1
+CERT_INSTALLED=$?
+
+if [ $CERT_INSTALLED -eq 0 ]; then
+    echo "Cert Already Installed"
+else
+    echo "Installing Cert"
+    sudo cp /vagrant/vagrantscripts/PCAcer.crt /usr/local/share/ca-certificates
+    sudo update-ca-certificates 
+fi
+
+docker ps > /dev/null 2>&1
+DOCKER_INSTALLED=$?
+
+if [ $DOCKER_INSTALLED -eq 0 ]; then
+    echo "Docker Already Installed"
+else
+    echo ">>> Installing docker"
+    apt-get update
+    sudo apt-get install -y docker.io
+    echo ">>> Adding vagrant user to docker group"
+    sudo usermod -aG docker vagrant
+    # Setup daemon.
+    bash -c 'cat > /etc/docker/daemon.json <<EOF
+    {
+    "exec-opts": ["native.cgroupdriver=systemd"],
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "100m"
+    },
+    "storage-driver": "overlay2"
+    }
+    EOF'
+
+    mkdir -p /etc/systemd/system/docker.service.d
+
+    # Restart docker.
+    systemctl daemon-reload
+    systemctl enable docker
+    systemctl restart docker
+fi
+
+kubeadm > /dev/null 2>&1
+KUBEADM_INSTALLED=$?
+
+if [ $KUBEADM_INSTALLED -eq 0 ]; then
+    echo "Kubeadm Already Installed"
+else
+    echo ">>> Installing Kubeadm"
+    apt-get update && apt-get install -y apt-transport-https curl
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+    bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+    apt-get update
+    apt-get install -y kubelet kubeadm kubectl
+    apt-mark hold kubelet kubeadm kubectl
+
+    # echo "Europe/Istanbul" | sudo tee /etc/timezone
+    ln -fs /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+    dpkg-reconfigure --frontend noninteractive tzdata
+
+    swapoff -a
+    sed -i '/ swap / s/^/#/' /etc/fstab    
+
+    echo ">>> INSTALLING kubens kubectx installation"
+    git clone https://github.com/ahmetb/kubectx /opt/kubectx
+    ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+    ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+    echo ">>> INSTALLING Helm !!!"
+    wget https://storage.googleapis.com/kubernetes-helm/helm-v2.13.1-linux-amd64.tar.gz
+    tar -zxf helm-v2.12.1-linux-amd64.tar.gz
+    mv linux-amd64/helm /usr/local/bin/helm
+    rm -rf linux-amd64 helm-v2.12.1-linux-amd64.tar.gz
+    
+    echo ">>> Time to do vagrant reload !!!"
+    # sudo shutdown now -r
+fi
+
